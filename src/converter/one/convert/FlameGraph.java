661aa91691bade818e9058ddae9134a03b5d1c8a
Peter Fraenkel <Peter.Fraenkel@morganstanley.com>
2025-04-23 11:10:33 -0400

    Add await frame types to jfr converter

--- src/converter/one/convert/FlameGraph.java
+++ src/converter/one/convert/FlameGraph.java
@@ -17,7 +17,7 @@ import static one.convert.ResourceProcessor.*;
 
 public class FlameGraph implements Comparator<Frame> {
     private static final Frame[] EMPTY_FRAME_ARRAY = {};
-    private static final String[] FRAME_SUFFIX = {"_[0]", "_[j]", "_[i]", "", "", "_[k]", "_[1]"};
+    private static final String[] FRAME_SUFFIX = {"_[0]", "_[j]", "_[i]", "", "", "_[k]", "_[1]", "_[a]", "_[a]"};
     private static final byte HAS_SUFFIX = (byte) 0x80;
     private static final int FLUSH_THRESHOLD = 15000;
     private static final Pattern TID_FRAME_PATTERN = Pattern.compile("\\[(.* )?tid=\\d+]");
